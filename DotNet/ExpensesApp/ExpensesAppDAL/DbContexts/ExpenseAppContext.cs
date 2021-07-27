e4724f5374decae829c25bb9111164b3478d35ba
Toni <toni.smolic89@gmail.com>
2021-07-27 10:37:35 +0200

    ver1

---
+++ DotNet/ExpensesApp/ExpensesAppDAL/DbContexts/ExpenseAppContext.cs
@@ -0,0 +1,24 @@
+﻿using ExpensesAppDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesAppDAL.DbContexts
+{
+    public class ExpenseAppContext : DbContext
+    {
+        public DbSet<ClientRole> ClientRoles { get; set; }
+
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public ExpenseAppContext(DbContextOptions<ExpenseAppContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TO-DO
+            //optionsBuilder.UseNpgsql()
+        }
+    }
+}
---
+++ DotNet/ExpensesApp/ExpensesAppDAL/Entities/ExpenseType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpensesAppDAL.Entities { 
+    [Table("expense_types")]
+    public class ExpenseType
+    {
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int ExpenseTypeID { get; set; }
+        [Column("description")] public string Description { get; set; }
+        [Column("expense_type")] public string IncomeOutcome { get; set; }
+        [Column("expected_expense")] public decimal ExpectedExpense { get; set; }
+        [Column("expense_notify")] public decimal ExpenseNotify { get; set; }
+        [Column("date_created")] public DateTime DateCreated { get; set; }
+        [Column("date_updated")] public DateTime DateUpdated { get; set; }
+    }
+}
